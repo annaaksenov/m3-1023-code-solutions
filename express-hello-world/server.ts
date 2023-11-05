fe916fa456436bed8ae62a8adfbfe32a76b6e5c2
Anna Aksenov <annaaksenov8@gmail.com>
2023-11-05 21:50:20 +0000

    Completed express-hello-world(1)

--- express-hello-world/server.ts
+++ express-hello-world/server.ts
@@ -1,14 +1,3 @@
-// import express, { Request, Response } from 'express';
-
-// const app = express();
-
-// app.use(function(req: Request, res: Response) {
-//   console.log(req.originalUrl);
-//   res.send('Hello again');
-// });
-
-// app.listen(8080, () => {console.log('Server is listening')});
-
 import express from 'express';
 const app = express();
 app.use(function (req, res) {
