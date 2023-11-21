800630995143529dd20dccc8d2993e689f4d5819
Anna Aksenov <annaaksenov8@gmail.com>
2023-11-21 06:09:01 +0000

    Completed react-carousel-component

---
+++ react-carousel-component/my-app/src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css';
+import Container from './Container';
+import './Container.css';
+
+const images = [
+  {
+    src: '/images/fushiguro.webp',
+    alt: 'Megumi Fushiguro',
+  },
+  {
+    src: '/images/inumaki.webp',
+    alt: 'Toge Inumaki',
+  },
+  {
+    src: '/images/itadori.webp',
+    alt: 'Yuji Itadori',
+  },
+  {
+    src: '/images/kugisaki.webp',
+    alt: 'Nobara Kugisaki',
+  },
+  {
+    src: '/images/panda.webp',
+    alt: 'Panda',
+  },
+  {
+    src: '/images/zen-in.webp',
+    alt: "Maki Zen'in",
+  },
+];
+
+function App() {
+
+  return (
+    <>
+      <Container images={images}/>
+    </>
+  )
+}
+
+export default App
